b0c944d555c6f96a8276818dbb0000ed13765a9d
xorgies <xorgies@gmail.com>
2023-03-22 21:35:22 +0100

    update web and using json data

--- web/index.py
+++ web/index.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+import json
 
 app = Flask(__name__)
 
@@ -8,7 +10,11 @@ def home():
 
 @app.route('/personajes')
 def personajes():
-    return render_template('personajes.html')
+    filename = os.path.join(app.static_folder, 'datos/datos.json')
+    with open(filename) as blog_file:
+        data = json.load(blog_file)
+
+    return render_template('personajes.html', jsonData=data)
 
 if __name__ == '__main__':
     # PRO
