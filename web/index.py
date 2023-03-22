0ba835a30de3c33ab94e45dfe11c54f1279de1b3
xorgies <xorgies@gmail.com>
2023-03-22 21:04:13 +0100

    web page skelleton

--- web/index.py
+++ web/index.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello world!'
+    return render_template('home.html')
+
+@app.route('/personajes')
+def personajes():
+    return render_template('personajes.html')
 
 if __name__ == '__main__':
-    app.run()
+    # PRO
+    # app.run()
+
+    # Testing
+    app.run(debug=True)
